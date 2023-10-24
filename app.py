3c82ac9ef2f2a795a18392f4e983d49575ed4970
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2023-10-24 19:15:31 +0530

    Task Monitoring project Started

---
+++ app.py
@@ -0,0 +1,27 @@
+# Library and Modules used 
+import os                                                    # pip install os ( In case if not present )
+import numpy as np                                           # pip install numpy ( In case if not present )
+import pandas as pd                                          # pip install pandas==1.4.3
+import openpyxl as oxl                                       # pip install openpyxl==3.0.10
+from tkinter import *                                        # pip install tkinter==8.6
+import customtkinter as ctk                                  # pip install customtkinter==4.6.3
+from PIL import Image ,ImageTk                               # pip install pillow==9.3.0
+import matplotlib.pyplot as plt                              # pip install matplotlib==3.4.3
+from tkinter.messagebox import showerror, showinfo
+
+class TaskMonitor :
+
+    def __init__() :
+        ctk.set_appearance_mode( "dark" )
+        ctk.set_default_color_theme( "dark-blue" )
+        self.width = 1200
+        self.height = 700
+        self.root = ctk.CTk()
+        self.root.title( "Task Monitor" )
+        self.root.geometry( "1200x700+200+80" )
+        self.root.resizable( False, False )
+
+if __name__ == "__main__" :
+
+    task_class = TaskMonitor()
+    task_class.firstPage()
