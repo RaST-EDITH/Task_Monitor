033cdf6f6f635a48ceaa3c22f21c709a3e778687
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2023-10-24 19:16:40 +0530

    First page structure created

--- app.py
+++ app.py
@@ -20,6 +20,17 @@ class TaskMonitor :
         self.root.title( "Task Monitor" )
         self.root.geometry( "1200x700+200+80" )
         self.root.resizable( False, False )
+    
+    def firstPage(self) :
+
+        # Defining Structure
+        first_page = Canvas( self.root, 
+                              width = self.width, height = self.height, 
+                               bg = "black", highlightcolor = "#3c5390", 
+                                borderwidth = 0 )
+        first_page.pack( fill = "both", expand = True )
+
+        self.root.mainloop()
 
 if __name__ == "__main__" :
 
